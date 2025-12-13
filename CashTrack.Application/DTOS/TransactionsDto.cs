abfa9d7c8245608cab84403ac471ffa9f40e0a52
Ayub-Tech <Ayubmohamud0920@gmail.com>
2025-12-13 14:02:37 +0100

    Add Transaction Dto

---
+++ CashTrack.Application/DTOS/TransactionsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashTrack.Application.DTOs
+{
+    public class TransactionDto
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
