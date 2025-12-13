9ea807fa036a99b926aca3abdea00dc211a640bc
Ayub-Tech <Ayubmohamud0920@gmail.com>
2025-12-13 14:00:35 +0100

    Add Transaction entity

---
+++ CashTrack.Domain/Entities/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashTrack.Domain.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
