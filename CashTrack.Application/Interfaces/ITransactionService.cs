62dde3bfdae16077d1b0c69bcf75ef09630cf1e6
Ayub-Tech <Ayubmohamud0920@gmail.com>
2025-12-13 14:27:23 +0100

    Connect TransactionService to interface

--- CashTrack.Application/Interfaces/ITransactionService.cs
+++ CashTrack.Application/Interfaces/ITransactionService.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace CashTrack.Application.Interfaces
 {
-    internal class ITransactionService
+    // Interface for transaction service
+    public interface ITransactionService
     {
+        // Will return all transactions later
+        void GetAll();
+
+        // Will create a new transaction later
+        void Create();
     }
 }
--- CashTrack.Application/Services/TransactionService.cs
+++ CashTrack.Application/Services/TransactionService.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CashTrack.Application.Interfaces;
 
 namespace CashTrack.Application.Services
 {
-    internal class TransactionService
+    // Service responsible for transaction-related logic
+    public class TransactionService : ITransactionService
     {
+        // Constructor (will be used later for dependencies)
+        public TransactionService()
+        {
+        }
+
+        // This method will later return all transactions
+        public void GetAll()
+        {
+            // Logic will be added later
+        }
+
+        // This method will later create a new transaction
+        public void Create()
+        {
+            // Logic will be added later
+        }
     }
 }
